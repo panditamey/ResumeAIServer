ce272a14265dd299c9f9cec3cde39305e4634d4c
panditamey <codewithamey@gmail.com>
2024-09-24 00:40:00 +0530

    CORS Resolved

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from utils import process_resume_and_jd
 from flask import request, render_template
 import os
@@ -6,7 +7,7 @@ import tempfile
 from flask import jsonify
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "*"}}) 
 
 @app.route('/')
 def home():
@@ -32,5 +33,81 @@ def magic():
     
     return jsonify(response_data)
 
+@app.route('/staticjson', methods=['GET'])
+def static_json():
+    return jsonify({
+    "certifications": [
+        {
+            "date": "",
+            "name": "Computer Hardware Certification"
+        },
+        {
+            "date": "",
+            "name": "Java Course"
+        }
+    ],
+    "education": [
+        {
+            "degree": {
+                "gpa": "7.94",
+                "name": "BE Computer Engineering",
+                "passing_year": "2024",
+                "university": "Mumbai University"
+            },
+            "high_school": {
+                "gpa": "85.37%",
+                "name": "Diploma In Computer Engineering",
+                "passing_year": "2021",
+                "university": "MSBTE University"
+            },
+            "school": {
+                "gpa": "59.23%",
+                "name": "HSC (Computer Science)",
+                "passing_year": "2019",
+                "university": "Konkan Board"
+            }
+        }
+    ],
+    "email": "panditsanket2211@gmail.com",
+    "experience": [
+        {
+            "company": "PHN Technology Pvt. Ltd.",
+            "description": "Assisted in web development projects using HTML, CSS, JavaScript, and PHP. Worked with MySQL databases.",
+            "end_date": "",
+            "location": "Pune",
+            "start_date": "",
+            "title": "Web Development Internship"
+        }
+    ],
+    "github": "",
+    "linkedin": "linkedin.com/in/sanket-pandit-7559861a3",
+    "location": "India",
+    "name": "Sanket Pandit",
+    "phone": "9405618195",
+    "projects": [
+        {
+            "description": "Performed end-to-end manual testing for a mock e-commerce website to validate functional requirements. Automated test cases for key features such as product search, shopping cart, and payment process using Selenium WebDriver and TestNG.",
+            "link": "",
+            "title": "E-Commerce Website Testing"
+        },
+        {
+            "description": "Conducted comprehensive API testing for a user authentication system, covering login, registration, password reset, and token validation features.",
+            "link": "",
+            "title": "API Testing of Authentication Application"
+        }
+    ],
+    "skills": [
+        "Testing Tools: Selenium, JUnit, TestNG, Postman, JIRA",
+        "Automation Frameworks: Selenium WebDriver, Cucumber",
+        "Manual Testing: Test Case Design, Regression Testing, UAT, Bug Tracking",
+        "SDLC and STLC",
+        "API Testing: REST API, Postman",
+        "Version Control: Git, GitHub",
+        "Test plan",
+        "CI/CD Tools: Jenkins (basic understanding)",
+        "SQL"
+    ],
+    "summary": "To achieve a responsible position and efficiently contribute to the industry by leveraging new technologies. Aiming to expand my knowledge through hard work and continuous learning."
+})
 if __name__ == '__main__':
     app.run(debug=True)
