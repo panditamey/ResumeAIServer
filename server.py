f36742855f27bae78de24972ab435e0e4180b74b
panditamey <codewithamey@gmail.com>
2024-09-23 23:31:58 +0530

    Seperated POST and GET

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from utils import process_resume_and_jd
 from flask import request, render_template
 import os
 import tempfile
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -11,26 +12,25 @@ app = Flask(__name__)
 def home():
     return 'Home Page Route'
 
-@app.route('/magic', methods=['GET', 'POST'])
+@app.route('/magic', methods=['GET'])
+def magic_form():
+    return "Resume Magic API"
+
+@app.route('/magic', methods=['POST'])
 def magic():
-    if request.method == 'POST':
-        resume_file = request.files['resume']
-        jd = request.form['jd']
-        additional_info = request.form['additional_info']
-        if additional_info == '':
-            additional_info = None
-        
-        with tempfile.NamedTemporaryFile(delete=False) as resume_temp:
-            resume_temp.write(resume_file.read())
-            resume_path = resume_temp.name
-            
-            response = process_resume_and_jd(resume_path, jd, 'pdf',  additional_info)
-        
-        os.remove(resume_path)
-        
-        return response
+    resume_file = request.files['resume']
+    jd = request.form.get('jd', 'Optimize Resume') 
+    additional_info = request.form.get('additional_info', None)  
     
-    return render_template('index.html')
+    with tempfile.NamedTemporaryFile(delete=False) as resume_temp:
+        resume_temp.write(resume_file.read())
+        resume_path = resume_temp.name
+        
+        response_data = process_resume_and_jd(resume_path, jd, 'pdf', additional_info)
+    
+    os.remove(resume_path)
+    
+    return jsonify(response_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
